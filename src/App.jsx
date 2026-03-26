472044c1bedd3a5111d72379d175387740f5ce7f
krishnakalathiya <krishnakalathiya03@gmail.com>
2026-03-26 14:10:24 +0530

    25-03-26

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>hello world</h1>
+        <h2>Hello World</h2>
+      </div>
+    </>
+  )
+}
+
+export default App
