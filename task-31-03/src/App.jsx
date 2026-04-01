75d864a3845927e9d317c82c4555d1f8d4479d0b
krishnakalathiya <krishnakalathiya03@gmail.com>
2026-04-01 14:13:29 +0530

    31-03

---
+++ task-31-03/src/App.jsx
@@ -0,0 +1,32 @@
+// 1. Fix an event handler
+// export default function LightSwitch() {
+//   function handleClick() {
+//     let bodyStyle = document.body.style;
+//     if (bodyStyle.backgroundColor === 'black') {
+//       bodyStyle.backgroundColor = 'white';
+//     } else {
+//       bodyStyle.backgroundColor = 'black';
+//     }
+//   }
+
+//   return (
+//     <button onClick={handleClick}>
+//       Toggle the lights
+//     </button>
+//   );
+// }
+
+
+// 2. Wire up the events
+export default function ColorSwitch({
+  onChangeColor
+}) {
+  return (
+    <button onClick={e => {
+      e.stopPropagation();
+      onChangeColor();
+    }}>
+      Change color
+    </button>
+  );
+}
