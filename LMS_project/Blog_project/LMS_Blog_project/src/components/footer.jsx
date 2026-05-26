910c3f2399dfc74e4e36c74a4fa2b79b8e1875e2
krishnakalathiya <krishnakalathiya03@gmail.com>
2026-05-26 15:01:15 +0530

    26-05

---
+++ LMS_project/Blog_project/LMS_Blog_project/src/components/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; {new Date().getFullYear()} Documentation Blog Project. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
